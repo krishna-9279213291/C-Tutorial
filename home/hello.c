7dc76cb86681ba95f43e9eb96751ed007326abff
Krishna Kumar <kk45859607@gmail.com>
2026-08-18 14:57:51 +0530

    First C program

---
+++ home/hello.c
@@ -0,0 +1,11 @@
+//Hello world using C progamming//
+
+// Just to give you a little excitement about C programming , I'm 
+// Going to give you a small conventional C programming Hello Program.
+ 
+#include <stdio.h>
+int main() {
+    // my first program in c
+    printf("Hello , World! \n");
+    return 0;
+}
